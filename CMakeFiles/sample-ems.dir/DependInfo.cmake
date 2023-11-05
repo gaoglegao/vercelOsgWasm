e70f6c7c7233dec584d633c71f08fb793dea44db
Your Name <you@example.com>
2023-11-05 22:09:38 +0800

    first init

---
+++ CMakeFiles/sample-ems.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/osgtest/official_osg_web_demo/openscenegraph-cross-platform-guide-application/web/src/main.cpp" "/mnt/d/osgtest/official_osg_web_demo/build/CMakeFiles/sample-ems.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/mnt/d/osgtest/official_osg_web_demo/openscenegraph-cross-platform-guide-application/web/../../OpenSceneGraph/include"
+  "/mnt/d/osgtest/official_osg_web_demo/openscenegraph-cross-platform-guide-application/web/../../OpenSceneGraph/build/Emscripten/include"
+  "/mnt/d/osgtest/official_osg_web_demo/openscenegraph-cross-platform-guide-application/web/src-gen"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/sample-ems.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample-ems.dir/src/main.cpp.o"
+  "sample-ems.html"
+  "sample-ems.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample-ems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
